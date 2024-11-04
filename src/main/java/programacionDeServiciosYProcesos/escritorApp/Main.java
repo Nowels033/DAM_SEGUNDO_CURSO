f5fc601bab1fa04cddbe42fc818b513217e06544
Nowels033 <noeldomse@gmail.com>
2024-11-04 16:01:19 +0100

    04/11/2024 16:00:00 Escritor

---
+++ src/main/java/programacionDeServiciosYProcesos/escritorApp/Main.java
@@ -0,0 +1,38 @@
+package programacionDeServiciosYProcesos.escritorApp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * Crea 5 hilos y lanza su ejecucion
+         */
+        int escritores = 1;
+//        Thread [] arrayEscritores = new Thread[5];
+//
+//        for (int i = 0; i < arrayEscritores.length; i++, escritores++) {
+//
+//        }
+//
+//        for (Thread arrayEscritore : arrayEscritores) {
+//
+//            Thread escritor = arrayEscritore;
+//
+//        }
+        Thread [] arrayEscritoresTotal = new Thread[7000];
+
+        for (int i = 0; i < arrayEscritoresTotal.length; i++) {
+            arrayEscritoresTotal[i] = new Thread(new Escritor("Escritor "+i));
+            arrayEscritoresTotal[i].start();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
