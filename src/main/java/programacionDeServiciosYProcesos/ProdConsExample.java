c52a81dade09aed9ad95b76d855922e178702ca4
Nowels033 <noeldomse@gmail.com>
2024-11-19 18:51:19 +0100

    04/11/2024 16:00:00 Escritor

--- src/main/java/programacionDeServiciosYProcesos/ProdConsExample.java
+++ src/main/java/programacionDeServiciosYProcesos/ProdConsExample.java
@@ -1,24 +1,226 @@
 package programacionDeServiciosYProcesos;
+/*
+
+
+import javax.swing.*;
 
 public class ProdConsExample {
-
-
     private static ListaEnteros listaEnteros = new ListaEnteros();
-     private static ListaEnteros monitor = new ListaEnteros();
+    private static ListaEnteros monitor = new ListaEnteros();
 
-    public static ListaEnteros getListaenteros() {
+    public static ListaEnteros getListaEnteros() {
         return listaEnteros;
     }
-
-
 
     public static ListaEnteros getMonitor() {
         return monitor;
     }
 
-    public static  void mostrarListaEnteros(){
+    public static void mostrarListaEnteros() {
+        if (listaEnteros.tam == 0) {
+            System.out.println("La lista enteros esta vacia[]");
+            return;
+        }
+
+        System.out.println("Contenido del array");
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+        int tam = listaEnteros.tam;
+        int[] arrayEnteros = listaEnteros.getArray();
+        for (int i = 0; i < tam; i++) {
+            System.out.print("[" + arrayEnteros[i] + "]");
+        }
+        System.out.println("");
+        vas a hacer algo?????
+        si no se que coño hacer puedes darme tu portatil =)
+        primero comeme el culo bien baboseado
+        yo te dejo imaginartelo a la perfeccion y ya me lo quedo
+        imagninado ya lo hice
+        solo queda ejecutarloejecutado
+        que pasa cotilla BRAYA, estoy viendo pedazo de monda
+
+
+
+
 
     }
+}
 
 
 }
+*/
